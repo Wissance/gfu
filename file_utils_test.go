4a7cc4b3cef52f08fe261342c280a16a507bb283
Ushakov Michale <um.nix.user@gmail.com>
2024-11-07 01:56:34 +0500

    added tests on ReadAllLines

--- file_utils_test.go
+++ file_utils_test.go
@@ -1,7 +1,91 @@
 package gfu_test
 
-import "testing"
+import (
+	"gfu"
+	"github.com/stretchr/testify/assert"
+	"github.com/wissance/gwuu/testingutils"
+	"path"
+	"testing"
+)
 
 func TestReadAllLine(t *testing.T) {
+	testCases := []struct {
+		name          string
+		fileName      string
+		omitEmpty     bool
+		expectedLines []string
+	}{
+		{
+			name:      "cr_ending_with_empty_lines_and_omit_empty_lines",
+			fileName:  path.Join(".", "testfiles", "cr_ending_with_empty_lines.txt"),
+			omitEmpty: true,
+			expectedLines: []string{
+				"Line one",
+				"Line two",
+				"Line three",
+				"Line four",
+				"{",
+				"    \"id\": 1,",
+				"    \"name\": \"Michael Ushakov\"",
+				"}",
+			},
+		},
+		{
+			name:      "lf_ending_with_empty_lines_and_omit_empty_lines",
+			fileName:  path.Join(".", "testfiles", "lf_ending_with_empty_lines.txt"),
+			omitEmpty: true,
+			expectedLines: []string{
+				"Line one",
+				"Line two",
+				"Line three",
+				"Line four",
+				"{",
+				"    \"id\": 1,",
+				"    \"name\": \"Michael Ushakov\"",
+				"}",
+			},
+		},
+		{
+			name:      "crlf_ending_with_empty_lines_and_omit_empty_lines",
+			fileName:  path.Join(".", "testfiles", "crlf_ending_with_empty_lines.txt"),
+			omitEmpty: true,
+			expectedLines: []string{
+				"Line one",
+				"Line two",
+				"Line three",
+				"Line four",
+				"{",
+				"    \"id\": 1,",
+				"    \"name\": \"Michael Ushakov\"",
+				"}",
+			},
+		},
+		{
+			name:      "crlf_ending_with_empty_lines_and_omit_empty_lines",
+			fileName:  path.Join(".", "testfiles", "crlf_ending_with_empty_lines.txt"),
+			omitEmpty: false,
+			expectedLines: []string{
+				"Line one",
+				"\t",
+				"Line two",
+				"Line three",
+				"\t",
+				"Line four",
+				"\t",
+				"{",
+				"    \"id\": 1,",
+				"    \"name\": \"Michael Ushakov\"",
+				"}",
+			},
+		},
+	}
 
+	for _, tCase := range testCases {
+		t.Run(tCase.name, func(t *testing.T) {
+			t.Parallel()
+			actualLines, err := gfu.ReadAllLines(tCase.fileName, tCase.omitEmpty)
+			assert.NoError(t, err)
+			testingutils.CheckStrings(t, tCase.expectedLines, actualLines, true, true)
+		})
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module gfu
 
 go 1.19
+
+require (
+	github.com/stretchr/testify v1.9.0
+	github.com/wissance/gwuu v1.4.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/wissance/stringFormatter v1.1.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
